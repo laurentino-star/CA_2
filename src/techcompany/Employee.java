7962703431d2132428eed0143e281400c0ac3068
Laurentino7 <189160695+Laurentino7@users.noreply.github.com>
2025-04-26 20:53:04 +0100

    Add Employee class
    Created Employee class with name, manager, and department fields. Included constructors, getters, and a toString() method for printing. Created to interface with input, sort, and search functionality.

--- src/techcompany/Employee.java
+++ src/techcompany/Employee.java
@@ -1,15 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package techcompany;
 
-/**
- *
- * @author laure f
- */
 public class Employee {
-    
+    private String name;
+    private Manager manager;
+    private Department department;
+
+    public Employee(String name, Manager manager, Department department) {
+        this.name = name;
+        this.manager = manager;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + manager.getTitle() + " - " + department.getName();
+    }
 }
-l
