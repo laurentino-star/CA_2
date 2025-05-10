59caaec3fb83a347c6947b32a8d84a97103f31e3
laurentino-star <150748386+laurentino-star@users.noreply.github.com>
2025-05-10 23:34:00 +0100

    Update Department.java/added comments

--- src/CA_2/Department.java
+++ src/CA_2/Department.java
@@ -1,13 +1,15 @@
-package CA_2;
+package CA_2; // Defines the package this class belongs to
 
-public class Department {
-    private String name;
+public class Department { // Declares the Department class
+    private String name; // Private field to store the name of the department
 
+    // Constructor to initialize the Department object with a name
     public Department(String name) {
-        this.name = name;
+        this.name = name; // Assigns the provided name to the name field
     }
 
+    // Getter method to retrieve the name of the department
     public String getName() {
-        return name;
+        return name; // Returns the name of the department
     }
 }
