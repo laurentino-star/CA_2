eed9060e6b5b10532d3ce62ddd6f4eec4e5218df
laurentino-star <150748386+laurentino-star@users.noreply.github.com>
2025-05-10 23:29:49 +0100

    Update AddRecordOption.java/ Added comments to the files

--- src/CA_2/AddRecordOption.java
+++ src/CA_2/AddRecordOption.java
@@ -1,6 +1,11 @@
+// Declares the package this enum belongs to (CA_2)
 package CA_2;
 
+// Defines an enum for different options when adding records
 public enum AddRecordOption {
+    // Represents the option to manually add a new employee record
     ADD_EMPLOYEE,
+    
+    // Represents the option to automatically generate a random employee record
     GENERATE_RANDOM_EMPLOYEE
 }
